facfccf4be7ce7d4cb3fc45d525c71585cc5604a
Nirmasiddh <nirmasiddh701@gmail.com>
2026-01-24 19:15:04 +0530

    Initial commit

--- MiniProject/app.js
+++ MiniProject/app.js
@@ -7,7 +7,9 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 //const { Hash } = require('crypto');
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -21,19 +23,64 @@ app.use(cookieParser());
 
 app.get('/', function(req,res){
     res.render('index.ejs')
-})
+});
 
 app.get('/login',  function(req,res){
     res.render('login')
-})
+});
 
-app.get('/profile', isLoggedIn,  function(req,res){
-    console.log(req.user);
-    res.render("login");
+app.get('/profile', isLoggedIn, async function(req,res){
+    let user = await userModel.findOne({email: req.user.email}).populate("posts");
 
-})
+    res.render("profile", {user});
+
+});
+
+app.get('/like/:id', isLoggedIn, async function(req,res){
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    if(post.likes.indexOf(req.user.userid) === -1 ){
+
+        post.likes.push(req.user.userid);
+
+    }else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+ 
+   await post.save();
+    res.redirect("/profile");
+});
+
+app.get('/edit/:id', isLoggedIn, async function(req,res){
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    
+    res.render("edit", {post});
+});
+
+app.post('/update/:id', isLoggedIn, async function(req,res){
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+    
+    res.redirect("/profile");
+});
+
+app.post('/post', isLoggedIn, async function(req,res){
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+
+    let post = await postModel.create({
+        user: user._id,
+        content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+
+    res.redirect('/profile');
+});
+
 
 app.post('/register', async function(req,res){
+
 
     let {username, name, age, email, password} = req.body;
 
@@ -70,7 +117,7 @@ app.post('/login', async function(req,res){
         if(result) {
             let token = jwt.sign({email:email, userid: user._id}, 'shhhh');
             res.cookie("token", token);
-            res.status(200).send("You can login");
+            res.status(200).redirect("/profile");
             
         }
         else res.redirect("/login");
@@ -84,7 +131,7 @@ app.get('/logout', function(req,res){
 })
 
 function isLoggedIn(req, res, next){
-    if(req.cookies.token === "") res.send("You Must be Logged in");
+    if(req.cookies.token === "") res.redirect("/login");
     else{
        let data = jwt.verify(req.cookies.token, "shhhh");
         req.user = data ;
--- MiniProject/models/user.js
+++ MiniProject/models/user.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
-
-const userSchema =  mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: String,
     name: String,
-    age: Number,
     email: String,
+    age: Number,
     password: String,
-    post: [
-        {
-            type:mongoose.Schema.Types.ObjectId, ref: "post"
-        }],
+
+    posts: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Post",
+        default: []   
+    }
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("User", userSchema);
